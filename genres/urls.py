b4be1a7d7ef2d30e2b6700d7123381ec0c4b9628
dezao <acoliveira87@gmail.com>
2024-08-17 18:55:58 -0300

    Configure urls

--- genres/urls.py
+++ genres/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+from . import views
 
 urlpatterns = [
-    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail'),
+    path('genres/', views.GenreCreateListView.as_view(), name='genre-create-list'),
+    path('genres/<int:pk>/', views.GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail'),
 ]
